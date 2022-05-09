5e8ed640cb540ebe463404516696b490f12a7bee
shaopopper <875799369@.qq.com>
2022-05-09 15:23:17 +0800

    2022-05-09 15:23

--- R/TCGA.R
+++ R/TCGA.R
@@ -63,6 +63,34 @@ cd <- function(xx){
   print(getwd())
 }
 
+#' describe_by
+#'
+#' 统计描述describe_by，搭配aggregate使用
+#'
+#' @param y 无意义
+#' @param na.omit 无意义
+#'
+#' @return
+#' @export
+#'
 
+describe_by<-function(y,na.omit=TRUE){
+  describe=function(x,na.omit=TRUE){   #na.omit=TRUE不忽略缺失指
+        if (na.omit)
+          Miss<-sum(is.na(x)==TRUE)   #统计缺失值
+        x<-x[!is.na(x)]  #去掉缺失值
+        m<-mean(x)
+        n<-length(x)  #不是缺失值的个数
+        s<-sd(x)
+        skew<-sum((x-m)^3/s^3)/n
+        kurt<-sum((x-m)^4/s^4)/n-3
+        #Normal_ks=ks.test(x,"pnorm")[["p.value"]]
+        Noraml_sh=shapiro.test(x)[["p.value"]]
+        return(round(c(n=n,Miss=Miss,mean=m,stdev=s,
+                       skew=skew,kurtosis=kurt,
+                       #Normal_ks=Normal_ks
+                       Noraml_sh=Noraml_sh),3))}
+        sapply(y,describe,na.omit=TRUE)
+}
 
 
